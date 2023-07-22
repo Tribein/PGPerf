17d9d272094eccddaf580e7a3679c0768779dd70
Tribein <tribein@gmail.com>
2023-07-22 19:48:03 +0300

    Merge origin/master

--- html/include.js
+++ html/include.js
@@ -32,7 +32,6 @@ function getData(query) {
 	var xmlHttp = new XMLHttpRequest();
 	var xmlHttpOutput;
 	xmlHttp.open("POST", ckhURL, false); // false for synchronous request
-	xmlHttp.setRequestHeader("Origin","*");
 	xmlHttp.send(query);
 	return xmlHttp.responseText;
 }
@@ -41,7 +40,6 @@ function a_getData(query,execfunc) {
 	var xmlHttp = new XMLHttpRequest();
 	var xmlHttpOutput;
 	xmlHttp.open("POST", ckhURL, true);
-	xmlHttp.setRequestHeader("Origin","*");
 	xmlHttp.send(query);
 	xmlHttp.onreadystatechange = function() { 
 		if (xmlHttp.readyState != 4) {return};
@@ -51,3 +49,32 @@ function a_getData(query,execfunc) {
 		}
 	}                
 }      
+
+function unflatten(arr) {
+    var tree = [],  mappedArr = {}, arrElem, mappedElem;
+    // First map the nodes of the array to an object -> create a hash table.
+    for (var i = 0, len = arr.length; i < len; i++) {
+        arrElem = arr[i];
+        mappedArr[arrElem.id] = arrElem;
+        mappedArr[arrElem.id]['children'] = [];
+    }
+    for (var id in mappedArr) {
+        if (mappedArr.hasOwnProperty(id)) {
+            mappedElem = mappedArr[id];
+            // If the element is not at the root level, add it to its parent array of children.
+            if (mappedElem.parentid) {
+				try{
+                        mappedArr[mappedElem.parentid]['children'].push(
+                                { "id": mappedElem.id, "parentid": mappedElem.parentid, "children": mappedElem.children }
+                        );
+                    tree.push(mappedElem);
+				}catch{}
+            }
+            // If the element is at the root level, add it to first level elements array.
+            else {
+                tree.push(mappedElem);
+            }
+        }
+    }
+    return tree;
+}
