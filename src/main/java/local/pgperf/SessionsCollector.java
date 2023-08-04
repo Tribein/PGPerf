899a0bd4e1325a892dd13a6b9b2ca1fe03566a63
Tribein <tribein@gmail.com>
2023-08-04 17:19:03 +0300

    Merge origin/master

--- src/main/java/local/pgperf/SessionsCollector.java
+++ src/main/java/local/pgperf/SessionsCollector.java
@@ -9,6 +9,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -24,7 +25,7 @@ public class SessionsCollector implements Configurable {
     private final String dbConnectionString;
     private final String dbUniqueName;
     private final String dbHostName;
-    private static final String PGSESSWAITSQUERY = 
+    private static final String PGSESSWAITSQUERY12 = 
         "select " +
             "coalesce(datname,'n/a') as datname, " +
             "pid, " +
@@ -50,7 +51,33 @@ public class SessionsCollector implements Configurable {
             "or state_change  > current_timestamp  - interval '"+SECONDSBETWEENSESSWAITSSNAPS+"' second"+
         ") and pid<>pg_backend_pid()"
         ;
-
+    private static final String PGSESSWAITSQUERY15 = 
+        "select " +
+            "coalesce(datname,'n/a') as datname, " +
+            "pid, " +
+            "0/*coalesce(leader_pid,0)*/ as leader_pid, " +
+            "coalesce((pg_blocking_pids(pid))[1],0) as blocked_by, " +
+            "coalesce(trim(application_name),'n/a') as application_name, " +
+            "coalesce(cast(client_addr as text),'n/a') as client_addr, " +
+            "coalesce(client_port,0) as client_port, " +
+            "backend_start, " +
+            "coalesce(xact_start,('2000-01-01 00:00:00+00'::timestamptz)) as xact_start, " +
+            "coalesce(query_start,('2000-01-01 00:00:00+00'::timestamptz)) as query_start, " +
+            "coalesce(state_change,('2000-01-01 00:00:00+00'::timestamptz)) as state_change, " +
+            "coalesce(wait_event_type,'n/a') as wait_event_type, " +
+            "coalesce(wait_event,'n/a') as wait_event, " +
+            "coalesce(state,'n/a') as state, " +
+            "coalesce(query_id,0) as query_id, " +
+            "backend_type, " +
+            "coalesce(cast(cast(backend_xid as text) as bigint),0) as backend_xid, " +
+            "coalesce(cast(cast(backend_xmin as text) as bigint),0) as backend_xmin " +
+        "from pg_catalog.pg_stat_activity "+
+        "where (" + 
+            "(coalesce(state,'-')<>'idle' and coalesce(wait_event,'-')<>'ClientRead') "+
+            "or state_change  > current_timestamp  - interval '"+SECONDSBETWEENSESSWAITSSNAPS+"' second"+
+        ") and pid<>pg_backend_pid()"
+        ;    
+    private static final String PGVERSIONQUERY = "select regexp_substr(version(),'\\d+',1,1)::int";
     public SessionsCollector(Connection connection, BlockingQueue<PgCkhMsg> queue, String dbname, String dbhost, String connstr) {
         ckhQueue                = queue;
         con                     = connection;
@@ -115,12 +142,48 @@ public class SessionsCollector implements Configurable {
             );
         }
     }
-
+    private int getVersion (Connection con) {
+            int version = 0;
+            ResultSet rs = null;
+            Statement stmt = null;
+            try{
+                stmt = con.createStatement();
+                rs = stmt.executeQuery(PGVERSIONQUERY);
+                if(rs.next()){
+                    version = rs.getInt(1);
+                }
+                rs.close();
+                stmt.close();
+                return version;
+            }catch(Exception e){
+                lg.LogError(DATEFORMAT.format(LocalDateTime.now()) + "\t" + dbConnectionString
+                    + "\t" + "cannot get version from database" 
+                    + "\t" + e.getMessage()
+                );                
+                if( rs != null || !rs.isClosed()){
+                    rs.close();
+                }
+                if ( stmt != null || ! stmt.isClosed()){
+                    stmt.close();
+                }
+                version = 0;
+            }finally{
+                return version;
+            }
+    }
     public void RunCollection() throws InterruptedException {
         PreparedStatement pgWaitsPreparedStatement=null;
-        boolean shutdown = false;        
+        boolean shutdown = false;    
+        
+        int version = getVersion(con);
+
         try {
-            pgWaitsPreparedStatement = con.prepareStatement(PGSESSWAITSQUERY);
+            pgWaitsPreparedStatement = 
+                    con.prepareStatement(
+                        (
+                                (version>=14)? PGSESSWAITSQUERY15 : PGSESSWAITSQUERY12
+                        )
+                    );
             pgWaitsPreparedStatement.setFetchSize(1000);
         } catch (SQLException e) {
             lg.LogError(DATEFORMAT.format(LocalDateTime.now()) + "\t" + dbConnectionString
